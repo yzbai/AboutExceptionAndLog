1f9052ad490b4a31cd2daa60b7d240250d497882
yzbai <yzbai@brlite.com>
2021-04-07 12:56:55 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AboutExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
